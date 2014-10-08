c46db3c41852e56f5f5b272abbab23d57656d017
Brent Knowles <knowedge@hotmail.com>
2014-10-08 14:41:26 -0600

    initial implementation complete

--- review.php
+++ review.php
@@ -4,6 +4,11 @@
 
 */
 class Review{
+
+	public static $REVIEW_FILE = "reviews.json";
+	public static $REVIEW_SCHEMA = "schema_reviews";
+	public static $REVIEW_COLUMNS = "columns_reviews";
+	
 	// Work Id associated with work (this connects the review to a specific work
 	public $work_id=-1;
 	// the caption heading
@@ -14,26 +19,32 @@ class Review{
 	public $review_site="";
 	// reviewers name
 	public $reviewer="";
+	// work name
+	public $workname="";
+	// work link
+	public $worklink="";
 	
-	function __construct($reviewcaption, $reviewtest, $workid, $reviewsite, $_reviewer) {
+	function __construct($reviewcaption, $reviewtest, $workid, $reviewsite, $_reviewer, $_workname, $_worklink) {
 	   $this->review_caption=$reviewcaption;
 	   $this->review_text=$reviewtest;
 	   $this->work_id = $workid;
 	   $this->review_site= $reviewsite;
 	   $this->reviewer = $_reviewer;
+	   $this->workname = $_workname;
+	   $this->worklink = $_worklink;
    }
 	////////////////////////////////////////
 	// Just randomizes the review list so the
 	// same review is not always shown.
 	////////////////////////////////////////
-    public function AdjustListOfReviews(&$reviews)
+    public static function AdjustListOfReviews(&$reviews)
 	{
 		shuffle($reviews);
 	}
 	////////////////////////////////////////
 	//
 	////////////////////////////////////////
- 	public function BuildStylesheet($reviews)
+ 	public static function BuildStylesheet($reviews)
 	{
 	
 		echo "<style>";
@@ -115,7 +126,7 @@ class Review{
 	////////////////////////////////////////
 	//
 	////////////////////////////////////////
-	public function BuildInputControls($reviews)
+	public static function BuildInputControls($reviews)
 	{
 		$count1 = 0;
 		foreach ($reviews as $review)
@@ -130,7 +141,7 @@ class Review{
 	////////////////////////////////////////
 	//
 	////////////////////////////////////////
-	public function BuildArticles($reviews)
+	public static function BuildArticles($reviews)
 	{
 	$count1 = 0;
 		foreach ($reviews as $review)
@@ -138,14 +149,14 @@ class Review{
 			$count1++;
 			$checked="";
 			
-			echo sprintf('<article><h2>%s</h3><h4> %s</h4><a href="%s">%s</a></article>',$review->review_caption,$review->review_text, $review->review_site, $review->reviewer);
+			echo sprintf('<article><h2>%s</h2><h4> %s</h4><a href="%s">%s</a> | <a href="%s">%s</a></article>',$review->review_caption,$review->review_text,$review->worklink,$review->workname, $review->review_site,  $review->reviewer);
 			echo "\n";
 		}
 	}
 	////////////////////////////////////////
 	//
 	////////////////////////////////////////
-	public function BuildControlLabels($reviews)
+	public static function BuildControlLabels($reviews)
 	{
 	$count1 = 0;
 	echo '	<div id=controls>';
@@ -165,7 +176,7 @@ class Review{
 	////////////////////////////////////////
 	//
 	////////////////////////////////////////
-	public function BuildActiveLabels($reviews)
+	public static function BuildActiveLabels($reviews)
 	{
 	$count1 = 0;
 	echo '<div id=active>';
@@ -182,6 +193,80 @@ class Review{
 		
 		echo '</div> <!-- #active --> ';
 	}
+	
+	
+	////////////////////////////////////////
+	// for the editor_dashboard; list of reviews
+	////////////////////////////////////////
+	public static function DrawListOfReviewsForEditing()
+	{
+	echo "<h1>Reviews</h1>";
+	 $result = json_decode(file_get_contents(Review::$REVIEW_FILE), true);
+	 
+	 $ListOfWorks = $result[util::$KEY_FOR_WORKSCREATED];
+	// if (!$ListOfWorks) echo "nothing";
+	 
+	 $results_array = array();
+	  foreach ( $ListOfWorks as $key_minor => $value_minor)
+	  {
+	 // echo "here";
+		  $warning="";
+		  $title = $result[util::$KEY_FOR_WORKSCREATED][$key_minor][util::$REVIEWTITLE];
+		  $workname = $result[util::$KEY_FOR_WORKSCREATED][$key_minor][util::$WORKNAME];
+		//echo "(".util::$REVIEWTITLE.") title = [".$title."] for key = ".$key_minor;
+		  
+		  // I put the ID = Title in here so that it would sort alphabetically.
+			$results_array[$title] =  sprintf("<h3> <span class='nicelink label label-default'><a id='%s' href='editor.php?record=%s&columns_to_use=%s&schema_to_use=%s&override_json=%s'>%s</a></span>%s|%s</h3>",$title,$key_minor,
+			Review::$REVIEW_COLUMNS,Review::$REVIEW_SCHEMA,Review::$REVIEW_FILE,
+			$workname,$title,$warning);
+	  }
+	  	  
+	  sort($results_array, SORT_STRING);
+	  
+		foreach ($results_array as $newkey=>$value)
+		{
+		
+			echo $value;
+		}
+	  
+	}
+	////////////////////////////////////////
+	// Load review file and reviews into array
+	////////////////////////////////////////
+	public static function LoadReviewsFromFile()
+	{
+		 $reviews = array();
+	// load file
+		$result = json_decode(file_get_contents(Review::$REVIEW_FILE), true);
+		 $ListOfWorks = $result[util::$KEY_FOR_WORKSCREATED];
+		if ($result)
+		{
+		
+			foreach ( $ListOfWorks as $key_minor => $value_minor)
+			{
+			
+			
+				$title=$result[util::$KEY_FOR_WORKSCREATED][$key_minor][util::$REVIEWTITLE];
+				if ($title && $title !== "")
+				{
+					
+					$blurb= $result[util::$KEY_FOR_WORKSCREATED][$key_minor][util::$REVIEWBLURB];
+					$workid=$result[util::$KEY_FOR_WORKSCREATED][$key_minor][util::$REVIEW_WORKID];
+					$reviewsite = $result[util::$KEY_FOR_WORKSCREATED][$key_minor][util::$REVIEWSITE];
+					$reviewname = $result[util::$KEY_FOR_WORKSCREATED][$key_minor][util::$REVIEWER];
+					$workname = $result[util::$KEY_FOR_WORKSCREATED][$key_minor][util::$WORKNAME];
+					$worklink = $result[util::$KEY_FOR_WORKSCREATED][$key_minor][util::$WORKLINK];
+					$review =  new Review($title,$blurb, $workid, $reviewsite, $reviewname, $workname, $worklink);
+					
+					array_push($reviews, $review);
+				}
+			}
+		 
+		
+		} 
+		 return $reviews;
+	}
+	
 }
 
 ?>
